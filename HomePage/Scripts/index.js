f863f12de41bbe604be6a3008c567cf1513077af
jack-burke-uk <80336264+jack-burke-uk@users.noreply.github.com>
2022-05-14 11:26:22 +0100

    Separate JSON for Project Data
    Keeping the project data in a separate JSON file so that I don't need to hog up all the space in index.js with values.
    
    Also, made the images slightly transparent so they change color as the page scrolls

--- HomePage/Scripts/index.js
+++ HomePage/Scripts/index.js
@@ -1,32 +1,20 @@
 vm = new Vue({
     el:"#app",
     data:{
-        projects:[
-            {
-                Title: "Duotone",
-                Description: "An easy way to apply theme consistency across a number of images",
-                Href: "/JavaScript/Duotone",
-                bgUrl:"./HomePage/Content/Images/duotone-transparent.png"
-            },
-            {
-                Title: "Dial Stats",
-                Description: "A javascript component for visualising percentage-based data",
-                Href: "/JavaScript/Dial%20Stat",
-                bgUrl:"./HomePage/Content/Images/dial-stat.png"
-            },
-            {
-                Title: "Spirograph",
-                Description: "A spirograph generator that creates a new image each time the page is loaded",
-                Href: "/JavaScript/Spirograph",
-                bgUrl:"./HomePage/Content/Images/spirograph.png"
-            },
-            {
-                Title: "Polaroid Video",
-                Description: "A JS page which can turn videos (or camera feeds) into a polaroid-style animation",
-                Href: "/JavaScript/Polaroid",
-                bgUrl:"./HomePage/Content/Images/polaroid.png"
+        isLoading: true,
+        projects:[]
+    },
+    created(){
+        var self = this;
+        var req = new XMLHttpRequest();
+        req.open("GET", "./HomePage/Content/Projects.json");
+        req.addEventListener("readystatechange", function(){
+            if(req.readyState == 4 && req.status == 200){
+                self.projects = JSON.parse(req.responseText).projects;
+                self.isLoading = false;
             }
-        ]
+        });
+        req.send();       
     },
     methods:{
         handleProjectClicked: function(project){
